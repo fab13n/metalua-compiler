fbe8db478c0f0012a64d3288ce6b8907e1b4725d
Fabien Fleutot <fleutot@gmail.com>
2014-01-09 01:26:04 +0100

    added a treequery usage sample

---
+++ samples/globals.lua
@@ -0,0 +1,80 @@
+-------------------------------------------------------------------------------
+-- Copyright (c) 2006-2014 Fabien Fleutot and others.
+--
+-- All rights reserved.
+--
+-- This program and the accompanying materials are made available
+-- under the terms of the Eclipse Public License v1.0 which
+-- accompanies this distribution, and is available at
+-- http://www.eclipse.org/legal/epl-v10.html
+--
+-- This program and the accompanying materials are also made available
+-- under the terms of the MIT public license which accompanies this
+-- distribution, and is available at http://www.lua.org/license.html
+--
+-- Contributors:
+--     Fabien Fleutot - API and implementation
+--
+-------------------------------------------------------------------------------
+
+--
+-- Example of treequery usage: list all the unauthorized global
+-- variable occurrences in a given source file.
+--
+-- Usage: lua globals.lua file_to_analyze.lua
+--
+
+require 'metalua.loader' -- needed to load "metalua/treequery.mlua"
+
+mlc     = require 'metalua.compiler'.new() -- compiler
+Q       = require 'metalua.treequery'      -- AST queries
+pp      = require 'metalua.pprint'         -- pretty-printer
+srcfile = ...                              -- filename to analyze...
+ast     = mlc :srcfile_to_ast(srcfile)     -- ...converted in an AST
+
+-- Global variable names which won't be reported:
+allowed = [[_G _VERSION assert collectgarbage coroutine debug dofile
+error gcinfo getfenv getmetatable io ipairs load loadfile loadstring
+math module newproxy next os package pairs pcall print rawequal rawget
+rawset require select setfenv setmetatable string table tonumber
+tostring type unpack xpcall]]
+
+-- The same, put in a hash-table for easy retrieval:
+allowed_set = { }
+for word in allowed :gmatch "%S+" do allowed_set[word]=true end
+
+function analyze(ast)
+    local globals = { } -- variable name -> list of line numbers
+
+    -- add the line number where node `id` occurs into table `globals`:
+    local function log_global(id)
+        local name, line = id[1], id.lineinfo.first.line
+        local lines = globals[name]
+        if lines then table.insert(lines, line)
+        else globals[name] = { line } end
+    end
+
+    -- Treequery command to retrieve global variable occurrences:
+    Q(ast)                         -- globals are...
+        :filter 'Id'               -- ...nodes with tag 'Id'...
+        :filter_not (Q.is_binder)  -- ...which are not local binders...
+        :filter_not (Q.get_binder) -- ...and aren't bound;
+        :foreach (log_global)      -- log each of them in `globals`.
+
+    -- format and sort the result
+    local sort_them = { }
+    for name, lines in pairs(globals) do
+        if not allowed_set[name] then
+            local line = name .. ": " .. pp.tostring(lines) :sub (2, -2)
+            table.insert(sort_them, line)
+        end
+    end
+
+    -- print the result
+    if next(sort_them) then
+        table.sort(sort_them)
+        print(table.concat(sort_them, "\n"))
+    else print "no global found" end
+end
+
+analyze(ast)
